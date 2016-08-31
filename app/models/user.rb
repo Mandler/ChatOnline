ef1b873f79554f7553b35126f30553426af79f5b
Rafal <rafal.pietrow@outlook.com>
2016-09-01 01:10:21 +0200

    first deployment of application

---
+++ app/models/user.rb
@@ -0,0 +1,10 @@
+class User < ApplicationRecord
+  has_many :chat_rooms, dependent: :destroy
+  has_many :messages, dependent: :destroy
+  devise :database_authenticatable, :registerable,
+  :recoverable, :rememberable, :trackable, :validatable
+
+  def name
+    email.split('@')[0]
+  end
+end
