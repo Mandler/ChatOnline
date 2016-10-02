18394fe7ed381b4ad4442ac88abd98939cf6923c
Rafal Pietrow <pietrow.raphael@gmail.com>
2016-10-03 00:06:04 +0200

    Add new field to user model (Devise)

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,7 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
   :recoverable, :rememberable, :trackable, :validatable
 
-  def name
-    email.split('@')[0]
-  end
+  # def name
+  #   email.split('@')[0]
+  # end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, :controllers => { registrations: 'registrations' }
 
   resources :chat_rooms, only: [:index, :new, :create, :show]
 
-  get '/display_chat_rooms', to: 'chat_rooms#display_chat_rooms', defaults: {format: :json}
+  get '/display_chat_rooms', to: 'chat_rooms#display_chat_rooms', defaults: { format: :json }
 
   mount ActionCable.server => '/cable'
 
