8a5cbaa7feef7f165d1bcf55424d0928cad60e45
Rafal Pietrow <pietrow.raphael@gmail.com>
2016-11-01 00:08:30 +0100

    Change creation of logs

--- config/application.rb
+++ config/application.rb
@@ -1,15 +1,15 @@
 require_relative 'boot'
 
-require "rails"
+require 'rails'
 
-require "active_model/railtie"
-require "active_job/railtie"
-require "active_record/railtie"
-require "action_controller/railtie"
-require "action_mailer/railtie"
-require "action_view/railtie"
-require "action_cable/engine"
-require "sprockets/railtie"
+require 'active_model/railtie'
+require 'active_job/railtie'
+require 'active_record/railtie'
+require 'action_controller/railtie'
+require 'action_mailer/railtie'
+require 'action_view/railtie'
+require 'action_cable/engine'
+require 'sprockets/railtie'
 # require "rails/test_unit/railtie"
 
 # Require the gems listed in Gemfile, including any gems
@@ -17,12 +17,20 @@ require "sprockets/railtie"
 Bundler.require(*Rails.groups)
 
 module ChatOnline
-  class Application < Rails::Application
+    class Application < Rails::Application
+        config.active_job.queue_adapter = :sidekiq
 
-    config.active_job.queue_adapter = :sidekiq
-    config.autoload_paths += %W(#{config.root}/lib)
-    # Settings in config/environments/* take precedence over those specified here.
-    # Application configuration should go into files in config/initializers
-    # -- all .rb files in that directory are automatically loaded.
-  end
+        # Warden create Log (Some user Logged in)
+        Warden::Manager.after_authentication do |user, auth, opts|
+            Log.create!(user_id: user.id, message: "#{user.email} Logged")
+        end
+
+        # Warden create Log (Some user Logout)
+        Warden::Manager.before_logout do |user, auth, opts|
+            Log.create!(user_id: user.id, message: "#{user.email} Logout")
+        end
+        # Settings in config/environments/* take precedence over those specified here.
+        # Application configuration should go into files in config/initializers
+        # -- all .rb files in that directory are automatically loaded.
+    end
 end
